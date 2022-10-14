f5c0c56ad4a5d22e635f37bd44de9b5777c1da9d
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-10-14 13:50:37 -0600

    App con Vite

---
+++ src/routes/Expenses.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Expenses = () => {
+    return (
+        <div>
+            <h2>Expenses</h2>
+        </div>
+    );
+};
---
+++ src/routes/Invoices.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Invoices = () => {
+    return (
+        <div>
+            <h2>Invoices</h2>
+        </div>
+    );
+};
